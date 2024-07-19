f914945601865134a346a6db52c05e774bbc88b0
cameron <aliyevcameron@gmail.com>
2024-07-19 10:51:47 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 
-function App() {
+import { useState } from 'react';
+import WeatherDetails from './components/WeatherDetails';
+import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch';
 
+const App = () => {
+  // src/App.jsx
+const [weather, setWeather] = useState({});
 
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
+  };
+  console.log(weather);
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <WeatherSearch fetchData={fetchData} />
+      <WeatherDetails weather={weather} />
+    </main>
   );
-}
+};
 
-export default App
+export default App;
